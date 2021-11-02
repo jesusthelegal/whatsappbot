9ba6bb2ef10a781f3844d00d67b5a2aa5d70a5d5
Jesu <92183493+jesusthelegal@users.noreply.github.com>
2021-11-03 00:55:01 +0200

    Add files via upload

---
+++ whatsapp.py
@@ -0,0 +1,13 @@
+import pywhatkit as kit
+
+number = "+90xxxxxxxxxx" # Telefon Numarası
+msg = "Bu bot Jesus tarafından kodlanmıştır." # Mesajınızı "" arasına girin tırnakları kesinlikle silmeyin
+hour = 17 # Saat
+minute = 23 # Dakika
+seconds = 30 # Saniye
+
+kit.sendwhatmsg(number, msg, hour, minute, seconds)
+
+
+
+# Bilgisiyarınızın Whatsapp Web e bağlı olduğundan emin olunuz.
